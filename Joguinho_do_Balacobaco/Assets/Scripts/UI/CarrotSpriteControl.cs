8fa7e8b263b39e119415502a5909923eceef66a7
Sannael <brunodonatonitoni@gmail.com>
2023-03-14 19:52:55 -0300

    Sistema de cenouras de vida automatizado

--- Joguinho_do_Balacobaco/Assets/Scripts/UI/CarrotSpriteControl.cs
+++ Joguinho_do_Balacobaco/Assets/Scripts/UI/CarrotSpriteControl.cs
@@ -5,17 +5,33 @@ using UnityEngine.UI;
 
 public class CarrotSpriteControl : MonoBehaviour
 {
-    public Sprite[] sprite;
-    public int spriteChange;
-    // Start is called before the first frame update
+    public Sprite[] sprite; //sprites da cenoura
+    public int spriteChange; //Id pra mudar a sprite
+    public int carrotId; //Id da carrot
+    private int health; //Vida do Player
+    public GameObject lifeManager; //Obj onde fica as barrinha de vida
+    
     void Start()
     {
-        
+        lifeManager = GameObject.Find("Life");
     }
 
     // Update is called once per frame
     void Update()
     {
+        health = GameObject.Find("Player").GetComponent<PlayerScript>().health;
+        if(health - (carrotId *2) > -1) //Se om resto de divisão for: > -1: manter o sprite da ultima cenoura como cenoura inteira
+        {
+            TrocaSprite(1);
+        }
+        else if(health - (carrotId *2) == -1) //Se for = -1: Mudar sprite da ultima cenoura pra cenoura "Mordida"
+        {
+            TrocaSprite(0);
+        }
+        else //se for < -1: Destroy a ultima cenoura
+        {
+            Destroy();
+        }
         
     }
 
@@ -31,5 +47,10 @@ public class CarrotSpriteControl : MonoBehaviour
         }
     }
 
-
+    public void Destroy()
+    {
+        Destroy(this.gameObject);
+        Destroy(lifeManager.GetComponent<LifeController>().hearts[carrotId]); //Destroy o objeto da cenoura armazenado no vetor (quando é criada)
+        lifeManager.GetComponent<LifeController>().count = carrotId -= 2; //Diminiu o contador de quantidade de cenouras
+    }
 }
